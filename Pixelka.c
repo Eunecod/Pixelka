c1d4aa38aa31c3f8751953cfeeadfcaba92bf1f2
Eugene Sudakov <sudakovzhenya02@gmail.com>
2025-01-27 20:30:02 +0700

    [feat]: Добавлена поддержка интерфейса I2C для взаимодействия с устройствами
    - Реализован код для работы с I2C, обеспечивающий взаимодействие с устройствами.
    - Обновлена система сборки для интеграции нового кода.

--- Pixelka.c
+++ Pixelka.c
@@ -1,31 +1,29 @@
-#include <avr/io.h>
 #include <util/delay.h>
-
-#define BAUD 9600
-#define UBRR (F_CPU / 16 / BAUD - 1)
+#include <drivers/i2c.h>
 
 
-void USART_Init(unsigned int ubrr) {
-    UBRR0H = (unsigned char)(ubrr >> 8);
-    UBRR0L = (unsigned char)ubrr;
-    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+void setup(void) {
+    I2C_Device myDevice;
+    I2C_Init(&myDevice);
+
+    I2C_Start();
+    I2C_Write(myDevice.slave_address << 1);
+    I2C_Write(0x31);
+    I2C_Write(0x41);
+    I2C_Stop();
+    
+    _delay_ms(1000); 
 }
 
-void USART_Transmit(unsigned char data) {
-    while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = data;
-}
-
-void USART_SendString(const char* str) {
-    while (*str) {
-        USART_Transmit(*str++);
-    }
+void loop(void) {
 }
 
 int main() {
-    USART_Init(UBRR);
-    USART_SendString("Hello, Arduino!\r\n");
+    setup();
+
+    for (;;) {
+        loop();
+    }
 
     return 0;
 }
--- include/drivers/i2c.h
+++ include/drivers/i2c.h
@@ -1,5 +1,22 @@
 #ifndef I2C_H
 #define I2C_H
 
+#include <avr/io.h>
+#include <util/twi.h>
+#include <util/delay.h>
+
+#define SLAVE_ADDRESS 0x33
+
+typedef struct {
+    uint8_t slave_address;
+} I2C_Device;
+
+void I2C_Init(I2C_Device* device);
+
+void I2C_Start(void);
+
+void I2C_Stop(void);
+
+void I2C_Write(uint8_t data);
 
 #endif // I2C_H
