26f80c64bbb9a6d5c9981ae54189c603626f098b
Eugene Sudakov <sudakovzhenya02@gmail.com>
2025-03-01 02:52:23 +0700

    [refactor] Оптимизация класса JoyInput

--- src/JoyInput.cpp
+++ src/JoyInput.cpp
@@ -2,51 +2,6 @@
 #include <JoyInput.h>
 
 
-//namespace JoyInput {
-//
-//	axis GetAxis(uint8_t joystick_x, uint8_t joystick_y) {
-//		axis _axis = { analogRead(joystick_x), analogRead(joystick_y) };
-//		return _axis;
-//	}
-//
-//	bool TriggerLeft(uint8_t joystick_x) {
-//		if (analogRead(joystick_x) < 300) {
-//			return true;
-//		}
-//
-//		return false;
-//	}
-//
-//	bool TriggerRight(uint8_t joystick_x) {
-//		if (analogRead(joystick_x) > 700) {
-//			return true;
-//		}
-//
-//		return false;
-//	}
-//
-//	bool TriggerUp(uint8_t joystick_y) {
-//		if (analogRead(joystick_y) > 700) {
-//			return true;
-//		}
-//
-//		return false;
-//	}
-//
-//	bool TriggerDown(uint8_t joystick_y) {
-//		if (analogRead(joystick_y) < 300) {
-//			return true;
-//		}
-//
-//		return false;
-//	}
-//
-//	bool Pushbutton(uint8_t joystick_button) {
-//		return digitalRead(joystick_button) == LOW;
-//	}
-//
-//} // JoyInput
-
 JoyInput::JoyInput(const uint8_t joystick_x, const uint8_t joystick_y, const uint8_t joystick_button) 
 	: joystick_x(joystick_x), joystick_y(joystick_y), joystick_button(joystick_button) {
 	
