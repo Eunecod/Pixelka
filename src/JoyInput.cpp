96413bb5b2cb8a94440e4cdb9147b49d5b300d5f
Eugene Sudakov <sudakovzhenya02@gmail.com>
2025-03-01 02:54:49 +0700

    [refactor] Оптимизация класса JoyInput

--- src/JoyInput.cpp
+++ src/JoyInput.cpp
@@ -17,10 +17,18 @@ axis JoyInput::GetAxis() const {
 bool JoyInput::Trigger(JoyInput::TRIGGER direction) const {
 	switch (direction)
 	{
-	case JoyInput::LEFT:	return analogRead(joystick_x) < 100;
-	case JoyInput::RIGHT:	return analogRead(joystick_x) > 900;
-	case JoyInput::UP:		return analogRead(joystick_y) < 100;
-	case JoyInput::DOWN:	return analogRead(joystick_y) > 900;
+	case JoyInput::LEFT:	
+		return analogRead(joystick_x) < 100;
+
+	case JoyInput::RIGHT:	
+		return analogRead(joystick_x) > 900;
+
+	case JoyInput::UP:		
+		return analogRead(joystick_y) < 100;
+
+	case JoyInput::DOWN:	
+		return analogRead(joystick_y) > 900;
+
 	default:
 		return false;
 	}
